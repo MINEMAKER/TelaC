d378a17c5417ff64f80ccd012b81a15e5092b764
MINEMAKER <otavioaugusto.lm@gmail.com>
2026-03-26 17:49:45 -0300

    Add color management functions and cleanup

--- telac.h
+++ telac.h
@@ -1,17 +1,37 @@
 #ifndef TELAC_H
 #define TELAC_H
 
-void get_resize(int sig);
-void tamanho_terminal(int *rows, int *cols);
-void ativar_alt(); //ativar tela alternativa
-void desativar_alt(); //desativar tela alternativa
-void gotoxy(int x, int y); //"cursor para (x, y)"
-void clear(); //limpar tela
-void hide_cursor(); //esconder cursor
-void show_cursor(); //mostrar cursor
-void desenhar_manual(int x, int y, int largura, int altura); //desenhar tela
-void desenhar_auto();
-char getch(); //identificar tecla pressionada
+void ativar_alt();
+void desativar_alt();
 
+typedef enum {
+    PRETO = 30,
+    VERMELHO = 31,
+    VERDE = 32,
+    AMARELO = 33,
+    AZUL = 34,
+    MAGENTA = 35,
+    CIANO = 36,
+    BRANCO = 37,
+    PADRAO = 0
+} Cor;
+
+void texto_cor(Cor cor);
+void fundo_cor(Cor cor);
+void print_cor(Cor cor, const char *formato, ...);
+
+void hide_cursor();
+void show_cursor();
+
+void clear();
+void gotoxy(int x, int y);
+
+void tamanho_terminal(int *rows, int *cols);
+void desenhar_borda(int rows, int cols);
+
+void init_terminal();
+void restore_terminal();
+
+char ler_tecla();
 
 #endif
