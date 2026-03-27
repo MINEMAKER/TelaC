7f3048cbbccdbd04dabe362ab65c4d68d9f2e155
MINEMAKER <otavioaugusto.lm@gmail.com>
2026-03-27 16:02:14 -0300

    Add terminal control macros and function declarations

--- telac.h
+++ telac.h
@@ -1,19 +1,40 @@
 #ifndef TELAC_H
 #define TELAC_H
+#include <unistd.h>
 
 void ativar_alt();
 void desativar_alt();
+#define INICIAR_TELA(titulo)                                                   \
+  int rows = 0, cols = 0;                                                      \
+  int rows_ant = 0, cols_ant = 0;                                              \
+  init_terminal();                                                             \
+  definir_titulo(titulo);                                                      \
+  while (1) {                                                                  \
+    if (ler_tecla() == 'q')                                                    \
+      break;                                                                   \
+    tamanho_terminal(&rows, &cols);                                            \
+    if (rows != rows_ant || cols != cols_ant) {                                \
+      clear();                                                                 \
+      desenhar_borda(rows, cols);                                              \
+      rows_ant = rows;                                                         \
+      cols_ant = cols;                                                         \
+    }
+
+#define FINALIZAR_TELA                                                         \
+  usleep(100000);                                                              \
+  }                                                                            \
+  restore_terminal();
 
 typedef enum {
-    PRETO = 30,
-    VERMELHO = 31,
-    VERDE = 32,
-    AMARELO = 33,
-    AZUL = 34,
-    MAGENTA = 35,
-    CIANO = 36,
-    BRANCO = 37,
-    PADRAO = 0
+  PRETO = 30,
+  VERMELHO = 31,
+  VERDE = 32,
+  AMARELO = 33,
+  AZUL = 34,
+  MAGENTA = 35,
+  CIANO = 36,
+  BRANCO = 37,
+  PADRAO = 0
 } Cor;
 
 void texto_cor(Cor cor);
@@ -25,6 +46,7 @@ void show_cursor();
 
 void clear();
 void gotoxy(int x, int y);
+void definir_titulo(const char *titulo);
 
 void tamanho_terminal(int *rows, int *cols);
 void desenhar_borda(int rows, int cols);
