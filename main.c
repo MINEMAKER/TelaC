1dc4e38937ecdeb4f3a99fb1e4e7dc22e1a2eb7b
MINEMAKER <otavioaugusto.lm@gmail.com>
2026-03-27 16:01:17 -0300

    Refactor main.c to simplify terminal interaction
    Removed terminal size handling and input loop.

--- main.c
+++ main.c
@@ -1,33 +1,13 @@
 #include "telac.h"
 #include <stdio.h>
-#include <unistd.h>
 
 int main() {
-  int rows = 0, cols = 0;
-  int rows_ant = 0, cols_ant = 0;
 
-  init_terminal();
+  INICIAR_TELA("Teste");
 
-  while (1) {
-    char tecla = ler_tecla();
+  gotoxy(5, 5);
+  printf("Teste");
 
-    if (tecla == 'q')
-      break;
-
-    tamanho_terminal(&rows, &cols);
-
-    if (rows != rows_ant || cols != cols_ant) {
-      clear();
-      desenhar_borda(rows, cols);
-      rows_ant = rows;
-      cols_ant = cols;
-    }
-
-    gotoxy(3, 3);
-    printf("Linhas: %d, Colunas: %d", rows, cols);
-    usleep(100000);
-  }
-
-  restore_terminal();
+  FINALIZAR_TELA
   return 0;
 }
