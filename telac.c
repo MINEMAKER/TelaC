64e07959d1d88e0a2dd86497d3624b9f82a99976
MINEMAKER <otavioaugusto.lm@gmail.com>
2026-04-07 14:45:56 -0300

    Refactor input handling and terminal functions

--- telac.c
+++ telac.c
@@ -8,9 +8,10 @@
 
 static struct termios oldt;
 
+int delay_ms = 100000;
 int rows = 0;
 int cols = 0;
-char tecla = 0;
+int tecla = 0;
 int tela = 0;
 int hover_menu = 0;
 
@@ -48,7 +49,6 @@ void mostrar_cursor() { printf("\033[?25h"); }
 
 void limpar() {
   printf("\033[2J\033[H");
-  desenhar_borda(rows, cols);
 }
 
 void gotoxy(int x, int y) { printf("\033[%d;%dH", y, x); }
@@ -77,52 +77,83 @@ void print_cor(Cor cor, const char *formato, ...) {
   texto_cor(PADRAO);
 }
 
+void resetar_cores() { printf("\033[0m"); }
+void texto_negrito() { printf("\033[1m"); }
+void texto_sublinhado() { printf("\033[4m"); }
+
+void definir_fps(int fps) {
+  if (fps > 0)
+    delay_ms = 1000000 / fps;
+}
+
+int obter_fps() {
+  if (delay_ms > 0) return 1000000 / delay_ms;
+  return 0;
+}
+
 // ---------- TERMINAL SIZE ----------
-void tamanho_terminal(int *rows, int *cols) {
+void atualizar_tamanho_terminal() {
   struct winsize w;
   ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
-  *rows = w.ws_row;
-  *cols = w.ws_col;
+  rows = w.ws_row;
+  cols = w.ws_col;
 }
 
 // ---------- INPUT ----------
-char ler_tecla() {
+int ler_tecla() {
   char c;
-  if (read(STDIN_FILENO, &c, 1) > 0)
+  if (read(STDIN_FILENO, &c, 1) > 0) {
+    if (c == '\033') {
+      char seq[2];
+      if (read(STDIN_FILENO, &seq[0], 1) == 0) return TECLA_ESC;
+      if (read(STDIN_FILENO, &seq[1], 1) == 0) return TECLA_ESC;
+
+      if (seq[0] == '[') {
+        if (seq[1] == 'A') return TECLA_CIMA;
+        if (seq[1] == 'B') return TECLA_BAIXO;
+        if (seq[1] == 'C') return TECLA_DIREITA;
+        if (seq[1] == 'D') return TECLA_ESQUERDA;
+      }
+      return TECLA_ESC;
+    }
+    if (c == '\r' || c == '\n') return TECLA_ENTER;
     return c;
+  }
   return 0;
 }
 
 // ---------- DESENHAR ----------
 void desenhar_borda(int rows, int cols) {
-  for (int y = 1; y <= rows; y++) {
-    printf("\033[%d;1H", y);
+  gotoxy(1, 1);
+  for (int x = 1; x <= cols; x++) printf("#");
+  
+  gotoxy(1, rows);
+  for (int x = 1; x <= cols; x++) printf("#");
 
-    for (int x = 1; x <= cols; x++) {
-      if (y == 1 || y == rows || x == 1 || x == cols)
-        printf("#");
-      else
-        printf(" ");
-    }
+  for (int y = 2; y < rows; y++) {
+    gotoxy(1, y);
+    printf("#");
+    gotoxy(cols, y);
+    printf("#");
   }
 }
 
 void desenhar_borda_xy(int x_inicial, int y_inicial, int x_final, int y_final) {
-  for (int y = y_inicial; y <= y_final; y++) {
-    for (int x = x_inicial; x <= x_final; x++) {
+  gotoxy(x_inicial, y_inicial);
+  printf("+");
+  for (int x = x_inicial + 1; x < x_final; x++) printf("-");
+  printf("+");
 
-      if (y == y_inicial || y == y_final || x == x_inicial || x == x_final) {
-        gotoxy(x, y);
+  gotoxy(x_inicial, y_final);
+  printf("+");
+  for (int x = x_inicial + 1; x < x_final; x++) printf("-");
+  printf("+");
 
-        if ((y == y_inicial || y == y_final) &&
-            (x == x_inicial || x == x_final))
-          printf("+");
-        else if (y == y_inicial || y == y_final)
-          printf("-");
-        else
-          printf("|");
-      }
-    }
+  for (int y = y_inicial + 1; y < y_final; y++) {
+    gotoxy(x_inicial, y);
+    printf("|");
+    gotoxy(x_final, y);
+    printf("|");
   }
 }
 
@@ -130,11 +161,11 @@ void criar_menu(int qtd_opcoes, ...) {
   if (tela != 0)
     return;
 
-  if (tecla == 'w' || tecla == 'W') {
+  if (tecla == 'w' || tecla == 'W' || tecla == TECLA_CIMA) {
     if (hover_menu > 0)
       hover_menu--;
   }
-  if (tecla == 's' || tecla == 'S') {
+  if (tecla == 's' || tecla == 'S' || tecla == TECLA_BAIXO) {
     if (hover_menu < qtd_opcoes - 1)
       hover_menu++;
   }
@@ -155,13 +186,13 @@ void criar_menu(int qtd_opcoes, ...) {
   }
   va_end(args);
 
-  if (tecla == '\n' || tecla == ' ') {
+  if (tecla == TECLA_ENTER || tecla == ' ') {
     tela = hover_menu + 1;
     limpar();
   }
 }
 
-void voltar_menu(char tecla_desejada) {
+void voltar_menu(int tecla_desejada) {
   if (tecla == tecla_desejada) {
     tela = 0;
     limpar();
