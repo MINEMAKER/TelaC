815b5838705f6c46d1bf868c47cad8337f847c5e
MINEMAKER <otavioaugusto.lm@gmail.com>
2026-04-07 14:46:16 -0300

    Update tecla type to int and add new functions

--- telac.h
+++ telac.h
@@ -2,11 +2,19 @@
 #define TELAC_H
 #include <unistd.h>
 
+extern int delay_ms;
 extern int rows;
 extern int cols;
-extern char tecla;
+extern int tecla;
 extern int tela;
 extern int hover_menu;
+
+#define TECLA_CIMA 1001
+#define TECLA_BAIXO 1002
+#define TECLA_DIREITA 1003
+#define TECLA_ESQUERDA 1004
+#define TECLA_ENTER 1005
+#define TECLA_ESC 27
 
 void ativar_alt();
 void desativar_alt();
@@ -15,18 +23,20 @@ void desativar_alt();
   init_terminal();                                                             \
   definir_titulo(titulo);                                                      \
   while (1) {                                                                  \
+    int resize_event = 0;                                                      \
     tecla = ler_tecla();                                                       \
     if (tecla == 'q')                                                          \
       break;                                                                   \
-    tamanho_terminal(&rows, &cols);                                            \
+    atualizar_tamanho_terminal();                                              \
     if (rows != rows_ant || cols != cols_ant) {                                \
       limpar();                                                                \
       rows_ant = rows;                                                         \
       cols_ant = cols;                                                         \
+      resize_event = 1;                                                        \
     }
 
 #define FINALIZAR_TELA                                                         \
-  usleep(100000);                                                              \
+  usleep(delay_ms);                                                            \
   }                                                                            \
   restore_terminal();
 
@@ -45,6 +55,12 @@ typedef enum {
 void texto_cor(Cor cor);
 void fundo_cor(Cor cor);
 void print_cor(Cor cor, const char *formato, ...);
+void resetar_cores();
+void texto_negrito();
+void texto_sublinhado();
+
+void definir_fps(int fps);
+int obter_fps();
 
 void esconder_cursor();
 void mostrar_cursor();
@@ -53,16 +69,16 @@ void limpar();
 void gotoxy(int x, int y);
 void definir_titulo(const char *titulo);
 
-void tamanho_terminal(int *rows, int *cols);
+void atualizar_tamanho_terminal();
 void desenhar_borda(int rows, int cols);
 void desenhar_borda_xy(int x_inicial, int y_inicial, int x_final, int y_final);
 
 void init_terminal();
 void restore_terminal();
 
-char ler_tecla();
+int ler_tecla();
 
 void criar_menu(int qtd_opcoes, ...);
-void voltar_menu(char tecla_desejada);
+void voltar_menu(int tecla_desejada);
 
 #endif
